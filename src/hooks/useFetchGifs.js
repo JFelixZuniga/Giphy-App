7331a277219e87c9b943cc0d913f50396d1865ff
JFelixZuniga <johnfzuniga@gmail.com>
2021-03-17 21:32:36 -0300

    App finalizada, pendiente CSS

---
+++ src/hooks/useFetchGifs.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { getGifs } from "../helpers/getGifs";
+
+export const useFetchGifs = (category) => {
+  const [state, setState] = useState({
+    data: [],
+    loading: true,
+  });
+
+  // useEffect -> nos permite ejecutar cierto código de manera condicional.
+  useEffect(() => {
+    getGifs(category).then((imgs) => {
+      setState({
+        data: imgs,
+        loading: false,
+      });
+    });
+  }, [category]);
+
+  return state; // {data: [], loading: true}
+};
